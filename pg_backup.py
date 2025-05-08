5d60e82e30bbf05ccebfa53b9524466052a768b4
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-05-08 13:49:47 -0500

    ruff format

--- pg_backup.py
+++ pg_backup.py
@@ -17,27 +17,37 @@ class Settings:
     version: str
 
     def __init__(self):
-        self.databases = os.getenv('DATABASES', '').split()
-        self.destination = pathlib.Path(os.getenv('DESTINATION')).resolve()
-        self.dsn = os.getenv('DSN')
-        self.log_format = os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
-        self.log_level = os.getenv('LOG_LEVEL', 'INFO')
-        self.version = '2024.3'
+        self.databases = os.getenv("DATABASES", "").split()
+        self.destination = pathlib.Path(os.getenv("DESTINATION")).resolve()
+        self.dsn = os.getenv("DSN")
+        self.log_format = os.getenv(
+            "LOG_FORMAT", "%(levelname)s [%(name)s] %(message)s"
+        )
+        self.log_level = os.getenv("LOG_LEVEL", "INFO")
+        self.version = "2024.3"
 
 
 def main():
     settings = Settings()
-    logging.basicConfig(format=settings.log_format, level='DEBUG', stream=sys.stdout)
-    logging.debug(f'pg-backup {settings.version}')
-    logging.debug(f'Changing log level to {settings.log_level}')
+    logging.basicConfig(format=settings.log_format, level="DEBUG", stream=sys.stdout)
+    logging.debug(f"pg-backup {settings.version}")
+    logging.debug(f"Changing log level to {settings.log_level}")
     logging.getLogger().setLevel(settings.log_level)
     for db in settings.databases:
-        backup_file = settings.destination / f'{datetime.date.today()}-{db}.sql'
-        logging.info(f'Backing up {db} to {backup_file}')
-        cmd = ['/usr/bin/pg_dump', '--clean', '--create', '--if-exists', '-d', f'{settings.dsn}/{db}',
-               '-f', str(backup_file)]
+        backup_file = settings.destination / f"{datetime.date.today()}-{db}.sql"
+        logging.info(f"Backing up {db} to {backup_file}")
+        cmd = [
+            "/usr/bin/pg_dump",
+            "--clean",
+            "--create",
+            "--if-exists",
+            "-d",
+            f"{settings.dsn}/{db}",
+            "-f",
+            str(backup_file),
+        ]
         subprocess.run(cmd)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
